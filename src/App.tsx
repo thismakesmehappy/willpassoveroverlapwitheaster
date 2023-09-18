5f84bbcd85255f9815c1e1ee6078bba9078663b8
Bernardo Margulis <bernardo@thismakesmehappy.co>
2023-09-18 16:12:35 -0400

    Calculate if the two holidays overlap

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,28 @@ import getPassoverForYear from "./helpers/getPassoverForYear.ts"
 
 import './App.css';
 import getEasterForYear from "./helpers/getEasterForYear.ts";
+import getDoEasterAndPassoverOverlap from "./helpers/getDoEasterAndPassoverOverlap.ts";
+
+const today: Date = new Date;
+const year = today.getFullYear() + 1;
 
 function App() {
-  const today: Date = new Date;
 
-const passoverDates = getPassoverForYear(today.getFullYear());
+const passoverDates = getPassoverForYear(year);
 console.log(passoverDates);
 
-const easter = getEasterForYear(today.getFullYear());
+const easter = getEasterForYear(year);
+
+const overlap = getDoEasterAndPassoverOverlap(passoverDates, easter);
 
   return (
     <>
         <div>
       <p>Passover {today.getFullYear()} falls between {passoverDates.start.toDateString()} and {passoverDates.end.toDateString()}</p>
         <p>Easter {today.getFullYear()} falls on {easter.toDateString()}</p>
+        </div>
+        <div>
+            In {today.getFullYear()}, Passover and Easter do {!overlap && "not"} overlap
         </div>
     </>
   )
