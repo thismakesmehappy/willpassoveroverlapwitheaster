8ce01e9251868851fd4a947b05da444cf17f956a
Bernardo Margulis <bernardo@thismakesmehappy.co>
2023-09-18 16:42:36 -0400

    Give status for next year

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,43 @@
-import getPassoverForYear from "./helpers/getPassoverForYear.ts"
+import getPassoverForYear from "./helpers/getPassoverForYear.ts";
 
-import './App.css';
+import "./css/style.css";
 import getEasterForYear from "./helpers/getEasterForYear.ts";
 import getDoEasterAndPassoverOverlap from "./helpers/getDoEasterAndPassoverOverlap.ts";
 
-const today: Date = new Date;
+const today: Date = new Date();
 const year = today.getFullYear() + 1;
 
 function App() {
+  const passoverDates = getPassoverForYear(year);
+  console.log(passoverDates);
 
-const passoverDates = getPassoverForYear(year);
-console.log(passoverDates);
+  const easter = getEasterForYear(year);
 
-const easter = getEasterForYear(year);
-
-const overlap = getDoEasterAndPassoverOverlap(passoverDates, easter);
+  const overlap = getDoEasterAndPassoverOverlap(passoverDates, easter);
 
   return (
     <>
-        <div>
-      <p>Passover {today.getFullYear()} falls between {passoverDates.start.toDateString()} and {passoverDates.end.toDateString()}</p>
-        <p>Easter {today.getFullYear()} falls on {easter.toDateString()}</p>
+      <div className="p-5 row align-items-center h3">
+        <div className="col col-6 text-center align-middle">
+          Passover {year} <br />
+          falls between
+          <br />
+          {passoverDates.start.toDateString()} <br />
+          and
+          <br />
+          {passoverDates.end.toDateString()}
         </div>
-        <div>
-            In {today.getFullYear()}, Passover and Easter do {!overlap && "not"} overlap
+        <div className="col col-6 text-center align-middle">
+          Easter {year} <br />
+          falls on
+          <br /> {easter.toDateString()}
         </div>
+        <div className={"text-center col-12 h1 p-4"}>
+          In {year}, Passover and Easter do {!overlap && "not"} overlap
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
