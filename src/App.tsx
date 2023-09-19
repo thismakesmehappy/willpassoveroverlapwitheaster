9ca6a4f619ef312096b8b5fc339956a547f09e87
Bernardo Margulis <bernardo@thismakesmehappy.co>
2023-09-18 20:22:45 -0400

    Extract DisplayList()

--- src/App.tsx
+++ src/App.tsx
@@ -3,40 +3,18 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 
 import getNextX from "./helpers/getNextX.ts";
 import DisplayFeatured from "./components/DisplayFeatured.tsx";
+import DisplayList from "./components/DisplayList.tsx";
 
 const numberOfYears = 10;
 
 function App() {
   const nextX = getNextX(numberOfYears);
 
-  const head = nextX[0];
-  const tail = nextX.slice(1);
-
   return (
     <>
-      <DisplayFeatured holidays={head} />
+      <DisplayFeatured holidays={nextX[0]} />
       <hr />
-      <div className="row">
-        {tail.map((item) => {
-          const year = item.year;
-          const passoverStart = item.passoverStart.toDateString();
-          const passoverEnd = item.passoverEnd.toDateString();
-          const easterDate = item.easterDate.toDateString();
-          const overlap = item.overlap;
-          return (
-            <div className="col col-12 col-md-6 col-lg-4 text-center">
-              In {year}, Easter and Passover do {!overlap && "not"} overlap
-              <br />
-              Passover start: {passoverStart}
-              <br />
-              Passover end: {passoverEnd} <br />
-              Easter: {easterDate}
-              <br />
-              ---
-            </div>
-          );
-        })}
-      </div>
+      <DisplayList listOfHolidays={nextX} />
     </>
   );
 }
--- src/helpers/getPassoverForYear.ts
+++ src/helpers/getPassoverForYear.ts
@@ -1,40 +1,40 @@
 // https://www.npmjs.com/package/@hebcal/core#HebrewCalendar.getHolidaysForYear
-import {HebrewCalendar} from "@hebcal/core";
+import { HebrewCalendar } from "@hebcal/core";
 
 export interface PassoverDates {
-    start: Date,
-    end: Date
+  start: Date;
+  end: Date;
 }
 
 const passoverStartDescription: string = "Erev Pesach";
-const passoverEndDescriotion: string = "Pesach VIII"
+const passoverEndDescriotion: string = "Pesach VIII";
 
 const getPassoverForYear = (year: number) => {
-    const options = {
-        year: year,
-        isHebrewHear: false
+  const options = {
+    year: year,
+    isHebrewYear: false,
+  };
+
+  let passoverStart: Date = new Date();
+  let passoverEnd: Date = new Date();
+
+  const holidaysForYear = HebrewCalendar.calendar(options);
+
+  holidaysForYear.forEach((event) => {
+    if (event.desc === passoverStartDescription) {
+      passoverStart = event.getDate().greg();
     }
+    if (event.desc === passoverEndDescriotion) {
+      passoverEnd = event.getDate().greg();
+    }
+  });
 
-    let passoverStart: Date = new Date();
-    let passoverEnd: Date = new Date();
+  const passoverDates = {
+    start: passoverStart,
+    end: passoverEnd,
+  } as PassoverDates;
 
-    const holidaysForYear = HebrewCalendar.calendar(options);
-
-    holidaysForYear.forEach(event => {
-        if (event.desc === passoverStartDescription) {
-            passoverStart = event.getDate().greg();
-        }
-        if (event.desc === passoverEndDescriotion) {
-            passoverEnd = event.getDate().greg();
-        }
-    })
-
-    const passoverDates = {
-        start: passoverStart,
-        end: passoverEnd
-    } as PassoverDates
-
-    return passoverDates;
-}
+  return passoverDates;
+};
 
 export default getPassoverForYear;
