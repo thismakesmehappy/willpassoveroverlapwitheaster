38ab615a50ef05c4a9e73cb2c95d81e07a5f4ee8
Bernardo Margulis <bernardo@thismakesmehappy.co>
2023-09-19 19:03:31 -0400

    Refactor format date short

--- src/helpers/formatDates.ts
+++ src/helpers/formatDates.ts
@@ -1,7 +1,17 @@
-import { days, months } from "./enums.ts";
+import { days, daysShort, months, monthsShort } from "./enums.ts";
 
 export const formatDatesLong = (date: Date) => {
   return (
     days[date.getDay()] + ", " + months[date.getMonth()] + " " + date.getDate()
+  );
+};
+
+export const formatDatesShort = (date: Date) => {
+  return (
+    daysShort[date.getDay()] +
+    ". " +
+    monthsShort[date.getMonth()] +
+    " " +
+    date.getDate()
   );
 };
