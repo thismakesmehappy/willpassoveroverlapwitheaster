6df5ec2a1ce1298337e3c520ddf9ece3d42b667d
Bernardo Margulis <bernardo@thismakesmehappy.co>
2023-09-24 14:54:26 -0400

    Remove limit to previous and next year

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,25 @@ import FeaturedYear from "./components/featured/FeaturedYear.tsx";
 import StandardList from "./components/standard/StandardList.tsx";
 import { useState } from "react";
 import { numberOfYears } from "./data/constants.ts";
+import getHolidaysForYear from "./helpers/getHolidaysForYear.ts";
+import getYearForNextHolidays from "./helpers/getYearForNextHolidays.ts";
 
 function App() {
   const [yearOffset, setYearOffset] = useState(0);
   const nextX = getNextX(numberOfYears);
-
+  const year = getYearForNextHolidays();
   const increaseYear = () => {
-    if (yearOffset < numberOfYears - 1) {
-      setYearOffset(yearOffset + 1);
-    }
+    setYearOffset(yearOffset + 1);
   };
 
   const decreaseYear = () => {
-    if (yearOffset > 0) {
-      setYearOffset(yearOffset - 1);
-    }
+    setYearOffset(yearOffset - 1);
   };
 
   return (
     <div className="container-responsive">
       <FeaturedYear
-        holidays={nextX[yearOffset]}
+        holidays={getHolidaysForYear(year + yearOffset)}
         isNext={yearOffset == 0}
         increaseYear={increaseYear}
         decreaseYear={decreaseYear}
