22197ee568a3e140665d8590012696615077b519
Bernardo Margulis <bernardo@thismakesmehappy.co>
2023-09-24 15:11:45 -0400

    Refactor featured year

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,14 @@ import "./css/style.css";
 import getNextX from "./helpers/getNextX.ts";
 import FeaturedYear from "./components/featured/FeaturedYear.tsx";
 import StandardList from "./components/standard/StandardList.tsx";
-import { useState } from "react";
 import { numberOfYears } from "./data/constants.ts";
-import getHolidaysForYear from "./helpers/getHolidaysForYear.ts";
-import getYearForNextHolidays from "./helpers/getYearForNextHolidays.ts";
 
 function App() {
-  const [yearOffset, setYearOffset] = useState(0);
   const nextX = getNextX(numberOfYears);
-  const year = getYearForNextHolidays();
-  const increaseYear = () => {
-    setYearOffset(yearOffset + 1);
-  };
-
-  const decreaseYear = () => {
-    setYearOffset(yearOffset - 1);
-  };
 
   return (
     <div className="container-responsive">
-      <FeaturedYear
-        holidays={getHolidaysForYear(year + yearOffset)}
-        isNext={yearOffset == 0}
-        increaseYear={increaseYear}
-        decreaseYear={decreaseYear}
-      />
+      <FeaturedYear />
       <hr />
       <StandardList listOfHolidays={nextX} />
     </div>
--- src/components/featured/FeaturedYear.tsx
+++ src/components/featured/FeaturedYear.tsx
@@ -1,24 +1,29 @@
-import { PassoverAndEaster } from "../../helpers/getHolidaysForYear.ts";
+import getHolidaysForYear, {
+  PassoverAndEaster,
+} from "../../helpers/getHolidaysForYear.ts";
 import FeaturedThreeHolidays from "./FeaturedThreeHolidays.tsx";
 import FeaturedYearHeader from "./FeaturedYearHeader.tsx";
 import PassoverAndEasterOverlap from "../PassoverAndEasterOverlap.tsx";
+import getYearForNextHolidays from "../../helpers/getYearForNextHolidays.ts";
+import { useState } from "react";
 
-interface Props {
-  holidays: PassoverAndEaster;
-  isNext: boolean;
-  decreaseYear: () => void;
-  increaseYear: () => void;
-}
-
-const FeaturedYear = ({
-  holidays,
-  isNext,
-  decreaseYear,
-  increaseYear,
-}: Props) => {
+const FeaturedYear = () => {
   const today: Date = new Date();
+  const year = getYearForNextHolidays();
+  const [yearOffset, setYearOffset] = useState(0);
+  const holidays = getHolidaysForYear(year + yearOffset);
+  const isNext = yearOffset == 0;
   const passed: boolean = isNext && today.getFullYear() < holidays.year;
 
+  const increaseYear = () => {
+    setYearOffset(yearOffset + 1);
+  };
+  const decreaseYear = () => {
+    setYearOffset(yearOffset - 1);
+  };
+  const resetYear = () => {
+    setYearOffset(0);
+  };
   return (
     <div className="p-5 row align-items-center text-center col col-12 col-md-10 col-lg-7 col-xl-6 m-auto featured-year">
       <FeaturedYearHeader
