2109e0cc07278b3cc3da528bc5dc3a4ef44da119
Bernardo Margulis <bernardo@thismakesmehappy.co>
2023-09-24 21:07:06 -0400

    Add header for future dates

--- src/components/standard/StandardList.tsx
+++ src/components/standard/StandardList.tsx
@@ -1,5 +1,6 @@
 import { PassoverAndEaster } from "../../helpers/getHolidaysForYear.ts";
 import StandardYear from "./StandardYear.tsx";
+import { numberOfYears } from "../../data/constants.ts";
 
 interface Props {
   listOfHolidays: PassoverAndEaster[];
@@ -7,21 +8,24 @@ interface Props {
 
 const StandardList = ({ listOfHolidays }: Props) => {
   return (
-    <div className="row">
-      {listOfHolidays.map((holiday) => {
-        return (
-          <div
-            className="col col-12 col-md-6 col-lg-4 col-xl-3 text-center"
-            key={"holiday" + holiday.year}
-          >
-            <StandardYear
-              holiday={holiday}
-              key={"standardyear" + holiday.year}
-            />
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div className="at-a-glance h2">{numberOfYears} years at a glance</div>
+      <div className="row">
+        {listOfHolidays.map((holiday) => {
+          return (
+            <div
+              className="col col-12 col-md-6 col-lg-4 col-xl-3 text-center"
+              key={"holiday" + holiday.year}
+            >
+              <StandardYear
+                holiday={holiday}
+                key={"standardyear" + holiday.year}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
