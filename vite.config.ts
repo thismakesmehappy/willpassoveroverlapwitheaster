b6cee338999ff31706cdec7455f705c190abc8bc
Bernardo Margulis <bernardo@thismakesmehappy.co>
2023-11-06 10:26:42 -0500

    Implement manual chunks to optimize bundle sizes

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,45 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "./",
+  build: {
+    rollupOptions: {
+      // preserveEntrySignatures: "exports-only",
+      output: {
+        minifyInternalExports: true,
+        compact: true,
+        // preserveModules: true,
+        manualChunks: {
+          dateholidays: ["date-holidays"],
+          dateholidaysparser: ["date-holidays-parser"],
+          moment: ["moment"],
+          momenttimezones: ["node_modules/moment-timezone/moment-timezone.js"],
+          momenttimezonespacked: [
+            "node_modules/moment-timezone/data/packed/latest.json",
+          ],
+          react: ["react", "react-dom"],
+          hebcal: ["@hebcal/core"],
+          dateholidayparserdeps: [
+            "caldate",
+            "astronomia",
+            "astronomia/data",
+            "date-bengali-revised",
+            "date-chinese",
+            "date-easter",
+            "deepmerge",
+          ],
+          dateholidaysparsersmall: [
+            "node_modules/date-holidays-parser/lib/internal/hebrew-calendar.cjs",
+            "node_modules/date-holidays-parser/lib/internal/utils.cjs",
+            "node_modules/date-holidays-parser/lib/Easter.cjs",
+            "node_modules/date-holidays-parser/lib/Holidays.cjs",
+            "node_modules/date-holidays-parser/src/Parser.js",
+            "node_modules/date-holidays-parser/src/Holidays.js",
+            "node_modules/date-holidays-parser/src/Data.js",
+            "node_modules/date-holidays-parser/src/Rule.js",
+            "node_modules/date-holidays-parser/src/utils.js",
+          ],
+        },
+      },
+    },
+  },
 });
